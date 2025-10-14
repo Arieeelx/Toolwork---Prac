81f806f3faee0e233544a4d4a9a1ba81d4d4e250
Devar <arielm.sereno.a@gmail.com>
2025-10-14 16:54:00 -0300

    Kupfer listo!

--- Scrapping Kupfer.py
+++ Scrapping Kupfer.py
@@ -7,29 +7,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 # ========================================
-# CATEGORIAS
-# ========================================
-
-url_categorias = {
-    #'seguridad_industrial': 'https://www.b2b.kupfer.cl/productos-con-acuerdo/seguridad-industrial.html?product_list_limit=36',
-    #'aceros_estructurales': 'https://www.b2b.kupfer.cl/aceros-estructurales.html?product_list_limit=36',
-    #'aceros_inoxidables_aluminio': 'https://www.b2b.kupfer.cl/aceros-inoxidables-y-aluminio.html?product_list_limit=36',
-    #'barras_acero': 'https://www.b2b.kupfer.cl/barras-de-acero.html?product_list_limit=36',
-    #'canerias': 'https://www.b2b.kupfer.cl/ca-erias.html?product_list_limit=36',
-    #'soldadura_corte': 'https://www.b2b.kupfer.cl/soldadura-y-corte.html?product_list_limit=36',
-    #'equipos_herramientas_maquinas_industriales': 'https://www.b2b.kupfer.cl/equipos-herramientas-y-maquinarias-industriales.html?product_list_limit=36',
-    #'mantencion_industrial': 'https://www.b2b.kupfer.cl/mantencion-industrial.html?product_list_limit=36',
-    #'izaje_traccion': 'https://www.b2b.kupfer.cl/izaje-y-traccion.html?product_list_limit=36',
-    'pesca_cultivo': 'https://www.b2b.kupfer.cl/pesca-y-cultivo.html?product_list_limit=36',
-    #'amarre': 'https://www.b2b.kupfer.cl/amarre.html?product_list_limit=36',
-    #'potencia_oleohidraulica': 'https://www.b2b.kupfer.cl/potencia-oleohidraulica.html?product_list_limit=36',
-    #'filtracion': 'https://www.b2b.kupfer.cl/filtracion.html?product_list_limit=36',
-    #'mangueras_conexiones': 'https://www.b2b.kupfer.cl/mangueras-y-conexiones.html?product_list_limit=36#',
-    #'manejo_fluido': 'https://www.b2b.kupfer.cl/productos-con-acuerdo/manejo-de-fluido.html?product_list_limit=36',
-    'equipos_energia': 'https://www.b2b.kupfer.cl/catalog/category/view/s/equipos-de-energia/id/1602/'
-}
-
-# ========================================
 # FUNCION PRINCIPAL SCRAP
 # ========================================
 
@@ -41,6 +18,8 @@ def scrap_kupfer(url, categoria_nombre):
     while True:
 
         elementos_listado = navegador.find_elements(By.CSS_SELECTOR, 'li.product-item')
+
+        print("=" * 70)
         print(f"[{categoria_nombre}] {len(elementos_listado)} Items encontrados")
 
         #CONTENIDO PARA REALIZAR EL SCRAPPING DE CADA ELEMENTO
@@ -76,25 +55,28 @@ def scrap_kupfer(url, categoria_nombre):
 
                 #SE GUARDAN EN VARIABLES
                 titulo_text = titulo_elemento.text if titulo_elemento else 'N/A'
-                sku_text = sku_elemento.text if sku_elemento else 'N/A'
+                sku_text = sku_elemento.text.replace('SKU: ', '').strip() if sku_elemento else 'N/A'
                 precio_oferta_text = precio_oferta_elemento.text if precio_oferta_elemento else 'No existe oferta'
                 precio_normal_text = precio_normal_elemento.text if precio_normal_elemento else 'Sin precio'
 
                 #SE AGREGA AL DATA LOS RESULTADOS
                 data.append({
-                    'Categoría': categoria_nombre,
-                    'Titulo': titulo_text,
                     'Sku': sku_text,
+                    'Titulo': titulo_text,
+                    'Categoría': categoria_nombre,
                     'Precio oferta': precio_oferta_text,
                     'Precio normal': precio_normal_text,
                     'Imagen': imagen if imagen else 'NA',
                 })
                 #AVISO DE QUE ARCHIVO SE VA AGREGANDO Y SUS PRECIOS RESPECTIVOS
-                print(f"->{titulo_elemento.text} || precio oferta: {precio_oferta_text} || precio normal: {precio_normal_text} || Sku: {sku_text}")
+                #print(f"->{titulo_elemento.text} || precio oferta: {precio_oferta_text} || precio normal: {precio_normal_text} || Sku: {sku_text}")
 
             except NoSuchElementException as e:
                 print(f"x Error: {e}")
                 continue
+
+        print(f" {len(data)} productos scrapeados de {categoria_nombre}")
+        print("=" * 70)
 
         #SENTENCIA PARA SCROLL DE PÁGINAS EN KUPFER
         try:
@@ -103,10 +85,34 @@ def scrap_kupfer(url, categoria_nombre):
             siguiente.click()
             time.sleep(1)
         except (NoSuchElementException, TimeoutException):
-            print(f"{categoria_nombre} <-- No hay más páginas para mostrar")
+            print("=" * 70)
+            print(f"{len(data)} Total productos scrapeados en {categoria_nombre} <- No existen más páginas para mostrar")
             break
     #IMPRIME EL VALOR A DATA
     return data
+
+# ========================================
+# CATEGORIAS
+# ========================================
+
+url_categorias = {
+    'seguridad_industrial': 'https://www.b2b.kupfer.cl/productos-con-acuerdo/seguridad-industrial.html?product_list_limit=36',
+    'aceros_estructurales': 'https://www.b2b.kupfer.cl/aceros-estructurales.html?product_list_limit=36',
+    'aceros_inoxidables_aluminio': 'https://www.b2b.kupfer.cl/aceros-inoxidables-y-aluminio.html?product_list_limit=36',
+    'barras_acero': 'https://www.b2b.kupfer.cl/barras-de-acero.html?product_list_limit=36',
+    'canerias': 'https://www.b2b.kupfer.cl/ca-erias.html?product_list_limit=36',
+    'soldadura_corte': 'https://www.b2b.kupfer.cl/soldadura-y-corte.html?product_list_limit=36',
+    'equipos_herramientas_maquinas_industriales': 'https://www.b2b.kupfer.cl/equipos-herramientas-y-maquinarias-industriales.html?product_list_limit=36',
+    'mantencion_industrial': 'https://www.b2b.kupfer.cl/mantencion-industrial.html?product_list_limit=36',
+    'izaje_traccion': 'https://www.b2b.kupfer.cl/izaje-y-traccion.html?product_list_limit=36',
+    'pesca_cultivo': 'https://www.b2b.kupfer.cl/pesca-y-cultivo.html?product_list_limit=36',
+    'amarre': 'https://www.b2b.kupfer.cl/amarre.html?product_list_limit=36',
+    'potencia_oleohidraulica': 'https://www.b2b.kupfer.cl/potencia-oleohidraulica.html?product_list_limit=36',
+    'filtracion': 'https://www.b2b.kupfer.cl/filtracion.html?product_list_limit=36',
+    'mangueras_conexiones': 'https://www.b2b.kupfer.cl/mangueras-y-conexiones.html?product_list_limit=36#',
+    'manejo_fluido': 'https://www.b2b.kupfer.cl/productos-con-acuerdo/manejo-de-fluido.html?product_list_limit=36',
+    'equipos_energia': 'https://www.b2b.kupfer.cl/catalog/category/view/s/equipos-de-energia/id/1602/'
+}
 
 # ========================================
 # INICIO DEL NAVEGADOR
@@ -129,8 +135,8 @@ try:
     email_input = wait.until(EC.presence_of_element_located((By.ID, "email")))
     password_input = navegador.find_element(By.ID, "pass")
 
-    email_input.send_keys("SSSS")
-    password_input.send_keys("SSS")
+    email_input.send_keys("ssss")
+    password_input.send_keys("ssss")
 
     login_button = navegador.find_element(By.CSS_SELECTOR, "button.action.login.primary")
     login_button.click()
@@ -151,8 +157,6 @@ except Exception as e:
     navegador.quit()
     exit()
 
-
-
 todos_los_datos = []
 
 print(f"INICIANDO SCRAPING DE {len(url_categorias)} CATEGORÍAS")
@@ -162,18 +166,22 @@ print(f"INICIANDO SCRAPING DE {len(url_categorias)} CATEGORÍAS")
 # ========================================
 
 for i, (categoria, url) in enumerate(url_categorias.items(), 1):
+    print("=" * 70)
     print(f"Scrapeando categoría: {categoria.upper()}")
     print(f"URL: {url}")
+    print("=" * 70)
 
     datos_categoria = scrap_kupfer(url, categoria)
     todos_los_datos.extend(datos_categoria)
-
-    print(f" {len(datos_categoria)} productos scrapeados de {categoria}")
 
 #GUARDAR DATOS CSV
 df = pd.DataFrame(todos_los_datos)
 df.to_csv('scrapping_Kupfer.csv', index=False, encoding='utf-8-sig')
 
-print(f"Total: {len(todos_los_datos)} productos scrapeados")
+print("\n" + "=" * 70)
+print(f"✓ SCRAPING COMPLETADO")
+print(f"✓ Total productos: {len(todos_los_datos)}")
+print(f"✓ Archivo: scrapping_Kupfer.csv")
+print("=" * 70 + "\n")
 
 navegador.quit()
